c4e4d7e758b3c57ca18b4e912aa3aaf03dd21b10
Goutham-09 <140509562+Goutham-09@users.noreply.github.com>
2026-09-01 22:35:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+sizeInfo = document.getElementById("sizeInfo").children[0]
+
+window.addEventListener("resize",function updatesize() {
+	sizeInfo.textContent = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+})
+updatesize()
+
